25e023604325ddc2d1205ac069c794d9767fdfb9
Natanael Lima <taelima1997@gmail.com>
2021-11-05 21:21:46 -0300

    add initial theme

---
+++ src/theme/light.ts
@@ -0,0 +1,17 @@
+const light = {
+    title: 'light',
+  
+    colors: {
+      primary: '#FB7750',
+      secundary: '#3E3E70',
+  
+      textBold: '#000000',
+      textRegular: '#4E4E4E',
+      gray: '#C4C4C4',
+      white: '#fff',
+    },
+  
+  };
+  
+  export default light;
+  
