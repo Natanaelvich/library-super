e3255c1ec892fa2c6af66cddbea39f8fcc13d246
Natanael Lima <taelima1997@gmail.com>
2021-11-05 21:20:20 -0300

    add types styled-components

---
+++ src/@types/styled-components.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import 'styled-components';
+import { ThemeType } from '../theme';
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
